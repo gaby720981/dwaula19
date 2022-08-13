7f5e09739547b80e585a5d3c08923c209c296614
AndreS27893 <andrescba27893@gmail.com>
2022-08-13 08:13:05 -0300

    Andres Bonilla

---
+++ Python/perro.py
@@ -0,0 +1,21 @@
+#Andres Bonilla
+dueno = [28957346,"Juan","Perez",4789689,"Belgrano 101"]
+if dueno[0] > 260000000: print(dueno[0])
+
+dueno2 = [23546987,"Maria","Perez",4789689,"Pueyrredon 811"]
+for element in dueno2:
+    print(element)
+
+perro = [13,"Puppy","12/12/2012","Macho",23546987]
+perro[2]= "13/12/2012"
+dueno[0] = 28957346
+
+HistorialTupla = (23500,5960,2300,10200,8900)
+def mayores():
+    for elementos in HistorialTupla:
+        print(elementos < 5000)
+        
+
+Clientes = ["Juan","Mario","Ariel","Josefina","Mariela"]
+Clientes.sort() 
+print(Clientes)      
